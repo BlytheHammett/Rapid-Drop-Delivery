f7267043911adf9e7326c7ff97203eaf0eb4ce78
BlytheHammett <hammettblythe@gmail.com>
2023-12-13 05:29:00 -0500

    change cors

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,14 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-app.use(cors())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://rapid-drop-delivery-cfbjf35ii-blythehammett.vercel.app/');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
